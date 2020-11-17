2a1aa8c38268d5937b7b7785ad936da6180640a1
Gabriel Eduardo Acosta Nuñez <gacosta@confisa.do>
2020-11-17 11:45:15 -0400

    Proyect in database sql

---
+++ MicroServicios/MicroServicio.Api.Autor/Models/AutorLibro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServicio.Api.Autor.Models
+{
+    public class AutorLibro
+    {
+        public int AutorLibroId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public ICollection<GradoAcademico> gradoAcademicos { get; set; }
+        public string AutorLibroGuid { get; set; }
+    }
+}
---
+++ MicroServicios/MicroServicio.Api.libro/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using MicroServicio.Api.libro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroServicio.Api.libro.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        public DbSet<LibreriaMaterial> libreriaMaterials { get; set; }
+    }
+}
